3a16a9db2f97c924fc7d730f3a1ab02c7514d654
vitalis <vitalismutwiri2gmail.com>
2023-01-11 21:44:06 +0300

    Testing using rspec

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,23 +1,12 @@
 require_relative '../person'
 describe Person do
-  context "when an instance created and parameter passed" do
-    person = Person.new(29,"mankush")
-    it "expect age to be 29" do
+  context 'when an instance created and parameter passed' do
+    person = Person.new(29, 'mankush')
+    it 'expect age to be 29' do
       expect(person.age).to eql 29
     end
-    it "expect name to be mankush" do
-      expect(person.name).to eql "mankush"
-    end
-  end
-endrequire_relative '../person'
-describe Person do
-  context "when an instance created and parameter passed" do
-    person = Person.new(29,"mankush")
-    it "expect age to be 29" do
-      expect(person.age).to eql 29
-    end
-    it "expect name to be mankush" do
-      expect(person.name).to eql "mankush"
+    it 'expect name to be mankush' do
+      expect(person.name).to eql 'mankush'
     end
   end
 end
