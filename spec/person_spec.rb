678916b652e34e36520e25e5bc918aa9b3923c9b
vitalis <vitalismutwiri2gmail.com>
2023-01-11 23:00:19 +0300

    Added rent_book and can_use_services ? tests

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,12 +1,25 @@
 require_relative '../person'
+require_relative '../rental'
+require_relative '../book'
+
 describe Person do
   context 'when an instance created and parameter passed' do
     person = Person.new(29, 'mankush')
+    book = Book.new 'benardo', 'bello'
+    rental = Rental.new '2023/01/01', book, person
+
     it 'expect age to be 29' do
       expect(person.age).to eql 29
     end
     it 'expect name to be mankush' do
       expect(person.name).to eql 'mankush'
+    end
+    it 'expect rental date to equal result date' do
+      result = book.rent_book('2023/01/01', book)
+      expect(result.date).to eq rental.date
+    end
+    it 'expect can_use_services to equal true' do
+      expect(person.can_use_services?).to eq true
     end
   end
 end
